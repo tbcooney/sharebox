6d648d572ce7e375a80dbe34010e1ad1625d86ac
tbcooney <taylor.cooney@vegastudios.ca>
2015-04-10 23:21:27 -0400

    Edit documents controller for form processing

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,13 +1,30 @@
 class DocumentsController < ApplicationController
   def index
+    @documents = Documents.all
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document.params)
+
+    if @doucment.save
+      redirect_to documents_path, notice: "The file #{@resume.name} has been successfully uploaded."
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @resume = Resume.find(params[:id])
+    @resume.destroy
+    redirect_to resumes_path, notice:  "The resume #{@resume.name} has been deleted."
+  end
+
+private
+  def document_params
+    params.requier(:document).permit(:name, :attachment)
   end
 end
